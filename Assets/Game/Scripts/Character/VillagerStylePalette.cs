5c362bb38b4f8d7bdc647abf614eae05846bf5ca
samoatesgames <sam@samoatesgames.com>
2020-04-18 13:26:49 +0100

    Initial Villager Creation.
     * Idle animation only.
     * Includes custom shader to randomize character clothing and hair.

---
+++ Assets/Game/Scripts/Character/VillagerStylePalette.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Villager Palette")]
+public class VillagerStylePalette : ScriptableObject
+{
+    public Color[] HairColors;
+    public Color[] TopColors;
+    public Color[] LegColors;
+}
---
+++ Assets/Game/Scripts/Character/VillagerStyleRandomizer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class VillagerStyleRandomizer : MonoBehaviour
+{
+    public VillagerStylePalette Palette;
+
+    private static readonly int HairColor = Shader.PropertyToID("_HairColor");
+    private static readonly int TopColor = Shader.PropertyToID("_TopColor");
+    private static readonly int LegColor = Shader.PropertyToID("_LegColor");
+
+    public void Start()
+    {
+        if (Palette.HairColors.Length == 0 || Palette.TopColors.Length == 0 || Palette.LegColors.Length == 0)
+        {
+            Debug.LogWarning($"The village '{name}' is missing one or more color override properties");
+            return;
+        }
+
+        var spriteRender = GetComponent<SpriteRenderer>();
+        var material = spriteRender.sharedMaterial;
+        material = new Material(material);
+        spriteRender.sharedMaterial = material;
+
+        material.SetColor(HairColor, Palette.HairColors[Random.Range(0, Palette.HairColors.Length)]);
+        material.SetColor(TopColor, Palette.TopColors[Random.Range(0, Palette.TopColors.Length)]);
+        material.SetColor(LegColor, Palette.LegColors[Random.Range(0, Palette.LegColors.Length)]);
+    }
+}
