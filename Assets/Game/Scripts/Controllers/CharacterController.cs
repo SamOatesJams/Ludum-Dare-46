1d60499de65a135691de38d31a782070e6b32aca
James Fitzpatrick <thefishlive@hotmail.com>
2020-04-18 08:58:36 +0100

    Add basic player input

---
+++ Assets/Game/Scripts/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+    private PlayerInput m_playerInput;
+    private InputAction m_movementAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerInput = GetComponent<PlayerInput>();
+        m_movementAction = m_playerInput.currentActionMap.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var movement = m_movementAction.ReadValue<Vector2>();
+        transform.position += new Vector3(movement.x, movement.y, 0);
+    }
+}
