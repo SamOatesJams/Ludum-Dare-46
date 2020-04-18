bfb05d680a7cb255113eaa6c5feefe4ef5403991
James Fitzpatrick <thefishlive@hotmail.com>
2020-04-18 21:01:16 +0100

    Apply changes to player prefab from dev scene

--- Assets/Game/Scripts/Environment/EnvironmentSurfaceData.cs
+++ Assets/Game/Scripts/Environment/EnvironmentSurfaceData.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(menuName = "Game/Environment/Surface Data")]
 public class EnvironmentSurfaceData : ScriptableObject
 {
     public string SurfaceName;
     public float MovementModifier;
+    public TileBase CollisionMapEntry;
 }
