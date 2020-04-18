2df8f5cb81cf507c9ffcc70ecce9849fa4e8f00a
samoatesgames <sam@samoatesgames.com>
2020-04-18 21:30:04 +0100

    Add a player map area collider used to limit the camera to the playable area.
    Added a shadow blob to the dr player

--- Assets/Game/Scripts/Camera/FollowTransform.cs
+++ Assets/Game/Scripts/Camera/FollowTransform.cs
@@ -11,20 +11,38 @@ public class SetCameraFollowTransformEvent : IEventAggregatorEvent
     }
 }
 
+public class SetCameraPlayableBoundsEvent : IEventAggregatorEvent
+{
+    public Bounds Bounds { get; }
+
+    public SetCameraPlayableBoundsEvent(Bounds bounds)
+    {
+        Bounds = bounds;
+    }
+}
+
 [DefaultExecutionOrder(-50)]
+[RequireComponent(typeof(Camera))]
 public class FollowTransform : SubscribableMonoBehaviour
 {
     public Vector3 Offset;
     public float Smoothness = 0.5f;
 
+    private Camera m_camera;
+
     private Transform m_targetTransform;
+    private Bounds? m_mapBounds;
+
     private Vector3 m_targetPosition;
     private Vector3 m_currentVelocity = Vector3.zero;
 
     public void Start()
     {
+        m_camera = GetComponent<Camera>();
+
         var eventAggregator = EventAggregator.GetInstance();
         eventAggregator.Subscribe<SetCameraFollowTransformEvent>(this, OnSetCameraFollowTransformEvent);
+        eventAggregator.Subscribe<SetCameraPlayableBoundsEvent>(this, OnSetCameraPlayableBoundsEvent);
     }
 
     private void OnSetCameraFollowTransformEvent(SetCameraFollowTransformEvent args)
@@ -32,6 +50,11 @@ public class FollowTransform : SubscribableMonoBehaviour
         m_targetTransform = args.Transform;
         m_targetPosition = m_targetTransform.position + Offset;
         transform.position = m_targetPosition;
+    }
+
+    private void OnSetCameraPlayableBoundsEvent(SetCameraPlayableBoundsEvent args)
+    {
+        m_mapBounds = args.Bounds;
     }
 
     public void Update()
@@ -42,6 +65,36 @@ public class FollowTransform : SubscribableMonoBehaviour
         }
 
         m_targetPosition = m_targetTransform.position + Offset;
-        transform.position = Vector3.SmoothDamp(transform.position, m_targetPosition, ref m_currentVelocity, Smoothness);
+
+        var oldPosition = transform.position;
+        transform.position = Vector3.SmoothDamp(oldPosition, m_targetPosition, ref m_currentVelocity, Smoothness);
+        
+        if (m_mapBounds.HasValue)
+        {
+            var mapBounds = m_mapBounds.Value;
+            var cameraBounds = CalculateCameraBounds();
+
+            if (!mapBounds.Contains(cameraBounds.min))
+            {
+                transform.position = oldPosition;
+            }
+
+            if (!mapBounds.Contains(cameraBounds.max))
+            {
+                transform.position = oldPosition;
+            }
+        }
+    }
+
+    private Bounds CalculateCameraBounds()
+    {
+        var screenAspect = Screen.width / Screen.height;
+        var cameraHeight = m_camera.orthographicSize * 2.0f;
+        var cameraPosition = m_camera.transform.position;
+
+        return new Bounds(
+            new Vector3(cameraPosition.x, cameraPosition.y, 0.0f), 
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0.0f)
+        );
     }
 }
---
+++ Assets/Game/Scripts/Environment/PlayableMapAreaBox.cs
@@ -0,0 +1,14 @@
+﻿using SamOatesGames.Systems;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayableMapAreaBox : MonoBehaviour
+{
+    public void Start()
+    {
+        var mapBounds = GetComponent<BoxCollider2D>().bounds;
+
+        var eventAggregator = EventAggregator.GetInstance();
+        eventAggregator.Publish(new SetCameraPlayableBoundsEvent(mapBounds));
+    }
+}
