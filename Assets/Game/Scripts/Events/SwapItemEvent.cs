9402cb937cdcd59dd36dc23cdce969c8cd1a46c1
Meepl <louis.harris1601@gmail.com>
2020-04-19 14:23:12 +0100

    Trap selection mostly
    clicking the ui selects a different trap, almost

---
+++ Assets/Game/Scripts/Events/SwapItemEvent.cs
@@ -0,0 +1,9 @@
+﻿using SamOatesGames.Systems;
+using UnityEngine;
+
+public class SwapItemEvent : IEventAggregatorEvent
+{
+    public GameObject item;
+    public ItemSelection itemSelection { get; set; }
+}
+
