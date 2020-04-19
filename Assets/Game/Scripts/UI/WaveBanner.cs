3099b151a4df9628a056a707347a47b73fc05e21
samoatesgames <sam@samoatesgames.com>
2020-04-19 09:19:15 +0100

    Add a wave banner which shows on wave start.

---
+++ Assets/Game/Scripts/UI/WaveBanner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using SamOatesGames.Systems;
+using TMPro;
+using UnityEngine;
+
+public class WaveBanner : SubscribableMonoBehaviour
+{
+    public UnityEngine.UI.Image BannerImage;
+    public TMP_Text BannerText;
+
+    public void Start()
+    {
+        var eventAggregator = EventAggregator.GetInstance();
+        eventAggregator.Subscribe<StartWaveEvent>(this, OnStartWaveEvent);
+
+        var bannerTransform = (RectTransform)BannerImage.transform;
+        bannerTransform.anchoredPosition = new Vector2(bannerTransform.anchoredPosition.x, 0.0f);
+    }
+
+    private void OnStartWaveEvent(StartWaveEvent args)
+    {
+        var bannerTransform = (RectTransform)BannerImage.transform;
+        StartCoroutine(MoveBanner(bannerTransform, bannerTransform.anchoredPosition.y, -bannerTransform.sizeDelta.y));
+    }
+
+    private IEnumerator MoveBanner(RectTransform bannerTransform, float startY, float endY)
+    {
+        var time = 0.0f;
+        while (time <= 1.0f)
+        {
+            var value = Mathf.Lerp(startY, endY, time);
+            bannerTransform.anchoredPosition = new Vector2(bannerTransform.anchoredPosition.x, value);
+
+            time += 0.01f;
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield return new WaitForSecondsRealtime(3.0f);
+
+        time = 0.0f;
+        while (time <= 1.0f)
+        {
+            var value = Mathf.Lerp(endY, startY, time);
+            bannerTransform.anchoredPosition = new Vector2(bannerTransform.anchoredPosition.x, value);
+
+            time += 0.01f;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
